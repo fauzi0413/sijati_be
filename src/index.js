9e2ccf5056a06f9e071a6ad32f7650c1ca214cfb
Fauzi <fauzi0413@gmail.com>
2025-07-17 21:47:03 +0700

    update url

--- src/index.js
+++ src/index.js
@@ -21,6 +21,18 @@ app.use('/chat-history', chatHistory);
 const faqManual = require('./faq_manual/faq_manual.controller.js');
 app.use('/faq-manual', faqManual);
 
+// USERS API
+const users = require('./users/user.controller.js');
+app.use('/users', users);
+
+// LOGIN LOGS API
+const login_logs = require('./loginLog/loginLog.controller.js');
+app.use('/login-logs', login_logs);
+
+// DOCUMENTS API
+const document = require('./document/document.controller.js');
+app.use('/document', document);
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
